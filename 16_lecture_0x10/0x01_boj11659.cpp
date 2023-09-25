a368c99a9a0e5b25d9fc0168f028f497b20ba06e
박창현 <86464544+Kid-Chang@users.noreply.github.com>
2023-09-25 20:10:43 +0900

    boj 문제번호 수정

--- 16_lecture_0x10/0x01_boj11659.cpp
+++ 16_lecture_0x10/0x01_boj11659.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-// 2×n 타일링
+// 구간 합 구하기 4
 
 // 조건
 // 
@@ -13,26 +13,28 @@ using namespace std;
 // 출력
 // 
 
-int n;
-int D[1001];
+int n, m;
+int D[100001];
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
     
-    cin >> n;
+    cin >> n >> m;
     
-    D[1]=1;
-    D[2]=2;
-
-    for(int i=3; i<=n;i++){
-        D[i] = (D[i-1] + D[i-2]) % 10007;
+    // 입력된 값들에 대한 합을 D[i] = D[i-1] + input_value;
+    for(int i=1;i<=n;i++){
+        int input_value;
+        cin >> input_value;
+        D[i] =  D[i-1] + input_value;
     }
-    // 주의사항
-    // 코드에서 별다른건 없는데 이런 문제에서 간혹 가다가 d[i] = d[i-1] + d[i-2]라고만 써놓고 제일 마지막에 d[i] % mod 를 출력하는 식으로 짜는 분이 있습니다. 그렇게 되면 계산 중간에 int overflow가 생겨서 틀리게 되니 
-    // 지금 이 코드처럼 계산 중간 중간에 계속 10007로 나눈 나머지만을 가져가도록 해야합니다.
     
-    cout << D[n];
+    // i~j까지의 합은 D[j]-D[i]
+    for(int i=0;i<m;i++){
+        int a,b;
+        cin >> a >> b;
+        cout << D[b]-D[a-1] << "\n";
+    }
     
 
     return 0;
